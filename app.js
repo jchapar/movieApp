59e205518276783f78908ecc98ade0129e108b18
Jose Chaparro <josechaparro@Joses-MacBook-Pro.local>
2021-04-11 17:30:16 -0700

    movie gathered from the api now appear in the DOM

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=45683cbb6a8af699dc7727a6b6fdd6f6&query="';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
-const seaarch = document.getElementById('search');
+const search = document.getElementById('search');
 
 // Get initial movies
 getMovies(API_URL);
@@ -12,8 +13,43 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data.results);
+    showMovies(data.results);
 }
+
+function showMovies(movies) {
+    main.innerHTML = '';
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie;
+
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+
+        movieElement.innerHTML = 
+        `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `;
+        main.appendChild(movieElement);
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+};
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
